e84e635c89e0aa61169fb8cff7a10c802e224045
Christian Braz <christiancleber@gmail.com>
2011-09-16 20:47:45 +0000

    Incluidas algumas opcoes para serem avaliadas se uteis posteriormente

--- facapedidos/application/Bootstrap.php
+++ facapedidos/application/Bootstrap.php
@@ -2,6 +2,44 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
 
+
+
+    /**
+         * Automatically load classes that are part of the default module.
+         */
+      /*  protected function _initModuleAutoloader()
+        {
+            new Zend_Application_Module_Autoloader(array(
+                'namespace' => 'Default',
+                'basePath' => APPLICATION_PATH
+            ));
+        } */
+
+        /**
+         * Initialize our routes.
+         * We setup a default route that allows the user to load an action like IndexController:testAction()
+         * through the URL like mysite.com/test. This allows us to not have to prefix the action with the
+         * controller like mysite.com/index/test. However, since the default route is there, that would
+         * still work and any other :controller/:action routes
+         */
+    
+     /*   protected function _initRoutes()
+        {
+            $this->bootstrap('frontcontroller');
+            $front = $this->getResource('frontcontroller');
+
+            $router = $front->getRouter();
+            $router->addRoute('index-action', new Zend_Controller_Router_Route(
+                ':action/*',
+                array(
+                    'controller' => 'index',
+                    'action' => 'index'
+                )
+            ));
+
+            return $router;
+        }  */
+
     protected function _initViews() {
         $this->bootstrap("view");
         $view = $this->getResource("view");
@@ -28,6 +66,53 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
 
         $front->registerPlugin(new Plugins_layout());
     }
+
+
+
+      /**
+         * Here we will initialize any view helpers.    This will also setup basic
+         * head information for the view/layout.
+         */
+     /*   protected function _initViewHelpers()
+        {
+            $this->bootstrap(array('frontcontroller', 'view'));
+            $frontController = $this->getResource('frontcontroller');
+            $view = $this->getResource('view');
+
+            // Add helper paths.
+            $view->addHelperPath(APPLICATION_PATH . '/views/helpers', 'Default_View_Helper');
+
+            // Setup our AssetUrl View Helper
+            if ((bool) $frontController->getParam('cdnEnabled'))
+                $view->getHelper('AssetUrl')->setBaseUrl($frontController->getParam('cdnHost'));
+
+            // Set our DOCTYPE
+            $view->doctype('XHTML1_STRICT');
+
+            // Set our TITLE
+            $view->headTitle()->setSeparator(' - ')->append('Site');
+
+            // Add any META elements
+            $view->headMeta()->appendHttpEquiv('Content-Type', 'text/html; charset=UTF-8');
+            $view->headMeta()->appendHttpEquiv('Content-Style-Type', 'text/css');
+            $view->headMeta()->appendHttpEquiv('imagetoolbar', 'no');
+
+            // Add our favicon
+            $view->headLink()->headLink(array(
+                'rel' => 'favicon',
+                'type' => 'image/ico',
+                'href' => $view->baseUrl('favicon.ico')
+            ));
+
+            // Add Stylesheet's
+            $view->headLink()
+                ->appendStylesheet($view->assetUrl('css/reset.css'));
+
+            // Add JavaScript's
+            $view->headScript()
+                ->appendFile('http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js');
+        }
+    */
 
 }
 
