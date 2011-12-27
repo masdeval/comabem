f348522e4748e7435aae71defab65d23e9d4374d
Christian Braz <christiancleber@gmail.com>
2011-12-27 13:35:23 +0000

    Incluir DAO no padrao do nome do arquivo

---
+++ facapedidos/application/models/DbTable/TipoProdutoDAO.php
@@ -0,0 +1,18 @@
+<?php
+
+class DbTable_TipoProduto extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'tipo_produto';
+    public function getCodTipoProductoDropDown()
+      {
+          $dat=$this->fetchAll();
+          foreach ($dat as $k=>$row) {
+              $result[$row->cod_tipo_produto] = $row->nome;
+          }
+          return $result;
+      }
+
+
+}
+
