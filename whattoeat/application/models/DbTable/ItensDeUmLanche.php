c53a747cff23e5cf8f8445a5dd141a7108f9a79c
masdeval@yahoo.com.br <masdeval@yahoo.com.br@af7db2a6-e711-49f0-bfed-15eee588241a>
2014-03-08 19:14:50 +0000

    Voltando com os controles que haviam sido desabilitados: hora de atendimento, categorias, etc

--- whattoeat/application/models/DbTable/ItensDeUmLanche.php
+++ whattoeat/application/models/DbTable/ItensDeUmLanche.php
@@ -1,7 +1,7 @@
 <?php
   class DbTable_ItensDeUmLanche extends Zend_Db_Table_Abstract
   {
-      protected $_name = 'itens_de_um_lanche';
+      protected $_name = 'ingrediente_empresa_produto';
 
     public function __construct($db)
     {
@@ -11,21 +11,21 @@
 
       public function getRecords($empresaId,$ingredienteId)
       {
-          $query = $this->_db->query("SELECT * FROM itens_de_um_lanche WHERE cod_empresa=$empresaId AND cod_ingrediente=$ingredienteId");
+          $query = $this->_db->query("SELECT * FROM ingrediente_empresa_produto WHERE cod_empresa=$empresaId AND cod_ingrediente=$ingredienteId");
 
           $result = $query->fetchAll();
           return $result;
       }
        public function getRecordsProduto($cod_empresa,$cod_produto)
       {
-          $query = $this->_db->query("SELECT * FROM itens_de_um_lanche WHERE cod_empresa=$cod_empresa AND cod_produto=$cod_produto");
+          $query = $this->_db->query("SELECT * FROM ingrediente_empresa_produto WHERE cod_empresa=$cod_empresa AND cod_produto=$cod_produto");
 
           $result = $query->fetchAll();
           return $result;
       }
        public function add($formData,$cod_produto,$cod_empresa)
       {
-          $this->_db->delete('itens_de_um_lanche', "cod_produto =$cod_produto and cod_empresa=$cod_empresa");
+          $this->_db->delete('ingrediente_empresa_produto', "cod_produto =$cod_produto and cod_empresa=$cod_empresa");
           foreach($formData['ie']['cod_ingrediente'] as $k=>$v){
               if(empty($v)){
                   continue;
@@ -42,14 +42,14 @@
       }
 
       public function getPreco($cod_produto){
-           $query = $this->_db->query("SELECT SUM(preco) as total FROM itens_de_um_lanche WHERE cod_produto=$cod_produto");
+           $query = $this->_db->query("SELECT SUM(preco) as total FROM ingrediente_empresa_produto WHERE cod_produto=$cod_produto");
 
           $result = $query->fetchAll();
           return $result[0]['total'];
       }
 
       public function getCalorias($cod_produto){
-           $query = $this->_db->query("SELECT SUM(valor_calorico) as total FROM itens_de_um_lanche WHERE cod_produto=$cod_produto");
+           $query = $this->_db->query("SELECT SUM(valor_calorico) as total FROM ingrediente_empresa_produto WHERE cod_produto=$cod_produto");
 
           $result = $query->fetchAll();
           return $result[0]['total'];
