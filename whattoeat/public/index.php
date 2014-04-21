07a982639042045a0776e7586e120fc42d85292a
augustosandim@gmail.com <augustosandim@gmail.com@af7db2a6-e711-49f0-bfed-15eee588241a>
2014-04-21 14:12:12 +0000

    Ajuste no layout, utilizando o framework bootstrap.

--- whattoeat/public/index.php
+++ whattoeat/public/index.php
@@ -22,7 +22,7 @@ set_include_path(implode(PATH_SEPARATOR, array(
 
 /** Zend_Application */
 require_once 'Zend/Application.php';
-require_once 'PagSeguroLibrary.php';
+//require_once 'PagSeguroLibrary.php';
 
 // Create application, bootstrap, and run
 $application = new Zend_Application(
